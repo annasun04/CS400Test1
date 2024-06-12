9c94952a9d72c689899cb66c7f279aff027c3810
annasun04 <asun47@wisc.edu>
2024-06-12 11:49:55 -0500

    Added a Printout.java file

---
+++ Printout.java
@@ -0,0 +1,6 @@
+// A test file for printing out a sentence
+public class Printout {
+	Public static void main(String [] args){
+		Ststem.out.println("Test");
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CS400Test1
 test repository
+Added a file called Printout.java
